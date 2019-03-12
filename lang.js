a575341388a8630975135b67376508b9aa384831
Benny Chak <bienfantaisie@gmail.com>
2019-03-13 03:42:42 +0800

    a
    a

--- lang.js
+++ lang.js
@@ -74,7 +74,7 @@ function switchlang(bl){
     la_65 : ['2016年12月','December 2016'],
     la_66 : ['顺风飞船一周年大数据','Trip Data on The 1\<sup\>st\<\/sup\> Anniversary Show'],
     la_69 : ['2016年6月','June 2016'],
-    la_70 : ['北京大学官方网站（国际版）','The International Official Website of Peking University'],
+    la_70 : ['北京大学官方网站（国际版）','The International Official Website'],
     la_71 : ['北京大学官方网站（国际版）于2015年11月20日改版上线。网站基于「面向未来的友好设计」设计思想，应用栅格布局和响应式设计方法，向前兼容IE7浏览器。','The International Official Portal Website of Peking University has made its debut on November 20\<sup\>th\<\/sup\> 2015\. This new responsive site is designed with \"Future-Friendly Design\" thinking\, applied bootstrap grid system\, and support IE7+ browser\.'],
     la_72 : ['UFO Animate','UFO Animate'],
     la_73 : ['个人项目','PERSONAL PROJECT'],
